4c067edcf432d0193a2ae4a6cd8836410c059b0d
elliotfouts <elliotfouts@gmail.com>
2020-04-04 09:47:38 -0700

    created server

--- server.js
+++ server.js
@@ -2,11 +2,16 @@
 const express = require("express");
 const htmlRoutes = require("./routes/htmlRoutes");
 const apiRoutes = require("./routes/apiRoutes");
+// instantiate express
 const app = express();
+// set port number
 const PORT = process.env.PORT || 4000;
 // configure the express app to serve static files and to handle data decoding 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+app.listen(()=>{
+    console.log(`Server listening at PORT: ${PORT}`);
+})
 
